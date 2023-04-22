90127c14120034eb81cdb0411a32e2f51ea74cff
+ <+>
2023-04-22 04:19:09 -0400

    why

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const replaceText = document.getElementById('text');
+    replaceText.textContent = 'This is really cool!'
+  });
+  
